e4668bc33cc514a13168a8c47c61f728f5d12196
brian070069 <kkipngeno409@gmail.com>
2024-07-21 16:04:00 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Wall wall = new Wall(5,4);
+        System.out.println("Area = "+ wall.getArea());
+
+        wall.setHeight(-1.5);
+        System.out.println("Height ="+wall.getHeight());
+    }
+}
